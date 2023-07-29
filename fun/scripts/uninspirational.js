3460cd40ad088a8d7a4b490c9dd19696cc53f931
bramblestars <txxcui@gmail.com>
2023-07-29 18:30:39 -0500

    add better documentation and prevent random quotes

--- fun/scripts/uninspirational.js
+++ fun/scripts/uninspirational.js
@@ -3,16 +3,31 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const newQuoteBtn = document.getElementById('new-quote');
 
+/**
+ * @returns a random quote from the list of quotes
+ */
 function randQuote() {
     return badQuotes[Math.floor(Math.random() * badQuotes.length)].text;
 }
 
+/**
+ * @returns a random author from the list of authors
+ */
 function randAuthor() {
     return fakeAuthors[Math.floor(Math.random() * fakeAuthors.length)].author;
 }
 
-function changeQuote() {
-    quote = randQuote(); 
+/**
+ * Changes current quote to a random new quote and randomizes the author
+ * Also change font-size of quote depending on its length
+ */
+function changeQuoteAndAuthor() {
+    quote = randQuote();
+
+    // Don't have a duplicate quote
+    while (quote === quoteText.textContent) {
+        quote = randQuote(); 
+    }
 
     quoteText.textContent = quote;
     authorText.textContent = randAuthor();
@@ -24,23 +39,26 @@ function changeQuote() {
     }
 }
 
-window.onload = changeQuote;
+window.onload = changeQuoteAndAuthor;
 
-newQuoteBtn.addEventListener('click', changeQuote);
+newQuoteBtn.addEventListener('click', changeQuoteAndAuthor);
 
 /**** Submit form toggle ****/
-const submitBtn = document.getElementById("submit-btn");
-const quoteTextArea = document.getElementById("quote-text");
-const submissionForm = document.getElementById("submission");
-const backBtn = document.getElementById("back-btn");
+const submitBtn = document.getElementById('submit-btn');
+const quoteTextArea = document.getElementById('quote-text');
+const submissionForm = document.getElementById('submission');
+const backBtn = document.getElementById('back-btn');
 
-function toggleForm () {
+/**
+ * Toggles between the submission form and the quotes page
+ */
+function toggleSubmitForm () {
     submissionForm.classList.toggle('submission-active');
     quoteTextArea.classList.toggle('nodisp');
 }
 
-submitBtn.addEventListener('click', toggleForm);
+submitBtn.addEventListener('click', toggleSubmitForm);
 
-backBtn.addEventListener('click', toggleForm);
+backBtn.addEventListener('click', toggleSubmitForm);
 
-submissionBtn.addEventListener('click', toggleForm);
+submitBtn.addEventListener('click', toggleSubmitForm);
