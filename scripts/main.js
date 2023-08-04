c0a2dfa993c89678ff9d651a8795247c6a4e461d
bramblestars <txxcui@gmail.com>
2023-08-04 02:57:04 -0500

    add game dev page

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,8 @@
-/************ Mobile button handler ************/
-
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const navEl = document.querySelector(".navigation");
 const badInspoEl = document.querySelector(".bad-inspo");
 
+/************ Mobile menu button handler ************/
 btnNavEl.addEventListener('click', function() {
     navEl.classList.toggle('nav-open');
 });
