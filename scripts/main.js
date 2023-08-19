9acab97768c3782aa2f40fd787dfd8d1274b5a0e
bramblestars <txxcui@gmail.com>
2023-08-19 04:29:48 -0500

    use jQuery

--- scripts/main.js
+++ scripts/main.js
@@ -20,20 +20,51 @@ const cvBtn = document.getElementById("cv");
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const navEl = document.querySelector(".navigation");
 
-
-
-/************ Mobile menu button handler ************/
+/***********************************************************/ 
+/************** Mobile menu button handler *****************/
+/***********************************************************/ 
 btnNavEl.addEventListener('click', function() {
     navEl.classList.toggle('nav-open');
 });
 
-/*************** Switch between pages ***************/
+/***********************************************************/ 
+/****************** Switch between pages *******************/
+/***********************************************************/ 
 let currentActivePage = aboutSection;
 
+/**
+ * Adds the nodisp class to the current active page and switches the current
+ * active page to the new section as specified. Also exits the mobile menu if
+ * that was active.
+ * @param {any} section the HTML section element to switch to 
+ */
+function switchToPage(section) {
+    currentActivePage.classList.add("nodisp");
+    section.classList.remove("nodisp");
+    currentActivePage = section;
+    currentActivePage.focus();
+    navEl.classList.remove('nav-open');
+}
 
-/******************** Dark theme ********************/
+$("#blog").click(() => {
+    switchToPage(blogSection);
+})
 
+$("#gamedev").click(() => {
+    switchToPage(gamedevSection);
+})
 
+$("#fun").click(() => {
+    switchToPage(funSection);
+})
+
+$("#cv").click(() => {
+    switchToPage(cvSection);
+})
+
+/***********************************************************/ 
+/************************ Dark theme ***********************/
+/***********************************************************/ 
 function nightTheme() {
     body.classList.toggle("dark");
 
@@ -53,33 +84,3 @@ if (localStorage.getItem('theme') && localStorage.getItem('theme') == 'dark') {
 }
 
 themeToggle.addEventListener('change', nightTheme);
-
-
-/**
- * Adds the nodisp class to the current active page and switches the current
- * active page to the new section as specified. Also exits the mobile menu if
- * that was active.
- * @param {any} section the HTML section element to switch to 
- */
-function switchToPage(section) {
-    currentActivePage.classList.add("nodisp");
-    section.classList.remove("nodisp");
-    currentActivePage = section;
-    navEl.classList.remove('nav-open');
-}
-
-blogBtn.addEventListener("click", () => {
-    switchToPage(blogSection);
-});
-
-gamedevBtn.addEventListener("click", () => {
-    switchToPage(gamedevSection);
-});
-
-funBtn.addEventListener("click", () => {
-    switchToPage(funSection);
-});
-
-cvBtn.addEventListener("click", () => {
-    switchToPage(cvSection);
-});
