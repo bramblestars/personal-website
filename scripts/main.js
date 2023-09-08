7c70aba182f4f50cacb56ea1a282b4f30c211d3f
bramblestars <txxcui@gmail.com>
2023-09-08 01:14:56 -0500

    finish hero

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
+// header
+const header = document.getElementById("header");
+
 // body (to add dark theme)
 const body = document.getElementById("body");
 
@@ -6,12 +9,10 @@ const themeToggle = document.querySelector('input[type="checkbox"]');
 
 // sections on home page
 const aboutSection = document.getElementById("about-section");
-const blogSection = document.getElementById("blog-section");
 const gamedevSection = document.getElementById("gamedev-section");
 const funSection = document.getElementById("fun-section");
 const cvSection = document.getElementById("cv-section");
 
-const blogBtn = document.getElementById("blog");
 const gamedevBtn = document.getElementById("gamedev");
 const funBtn = document.getElementById("fun");
 const cvBtn = document.getElementById("cv");
@@ -20,12 +21,38 @@ const cvBtn = document.getElementById("cv");
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const navEl = document.querySelector(".navigation");
 
+// keyboard event codes
+const KEY_SPACE = 32;
+const KEY_ENTER = 13;
+
+// focus booleans
+let isFocusOnToggle = false;
+
 /***********************************************************/ 
 /************** Mobile menu button handler *****************/
 /***********************************************************/ 
 btnNavEl.addEventListener('click', function() {
     navEl.classList.toggle('nav-open');
 });
+
+/***********************************************************/ 
+/********************* Sticky header ***********************/
+/***********************************************************/ 
+// When the user scrolls the page, execute myFunction
+window.onscroll = stickyHeader;
+
+
+// Get the offset position of the navbar
+const sticky = $(window).height();
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyHeader() {
+  if (window.scrollY > sticky) {
+    body.classList.add("sticky-header");
+  } else {
+    body.classList.remove("sticky-header");
+  }
+} 
 
 /***********************************************************/ 
 /****************** Switch between pages *******************/
@@ -46,41 +73,46 @@ function switchToPage(section) {
     navEl.classList.remove('nav-open');
 }
 
-$("#blog").click(() => {
-    switchToPage(blogSection);
-})
 
 $("#gamedev").click(() => {
     switchToPage(gamedevSection);
-})
+});
 
 $("#fun").click(() => {
     switchToPage(funSection);
-})
+});
 
 $("#cv").click(() => {
     switchToPage(cvSection);
-})
+});
 
 /***********************************************************/ 
 /************************ Dark theme ***********************/
 /***********************************************************/ 
-function nightTheme() {
+function nightTheme(event) {
     body.classList.toggle("dark");
 
     // Save preference to local storage
     if (body.classList.contains("dark")) {
         localStorage.setItem('theme', 'dark');
+        themeToggle.checked = true;
     } else {
         localStorage.setItem('theme', 'light');
+        themeToggle.checked = false;
     }
 }
 
 if (localStorage.getItem('theme') && localStorage.getItem('theme') == 'dark') {
-    themeToggle.checked = true;
     nightTheme();
-} else {
-    themeToggle.checked = false;
-}
+} 
 
 themeToggle.addEventListener('change', nightTheme);
+$("#theme-toggle").keydown((event) => {
+    if (isFocusOnToggle && event.keyCode === KEY_ENTER) {
+        nightTheme();
+    }
+});
+
+function setFocusThemeToggle() {
+    isFocusOnToggle = true;
+}
